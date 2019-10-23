9b90ab55fa1c3ffd5c4dfd923f2fa49ca4fb5931
lt1029re <lt1029re@ed.ritsumei.ac.jp>
2019-10-23 05:16:07 +0000

    Some fix

---
+++ kadai1023/index.js
@@ -0,0 +1,13 @@
+const firstName = 'miku'
+const secondName = 'sakata'
+console.log(firstName+secondName)
+const sum =1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const hasCat = false
+const hasDog = true
+
+console.log(hasDog || hasCat)
+console.log(hasDog && hasCat)
+//犬も猫も買っていない
+console.log(!hasDog && !hasCat)
